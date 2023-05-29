b79d8ec7465bbe2f251da2bf0510c0904dc15b92
Sarper Turan <sarper.turan01@gmail.com>
2023-05-30 00:50:02 +0300

    Add Pub/Sub to the project

---
+++ server/src/main/java/com/example/demo/pubsub/OrderPublisher.java
@@ -0,0 +1,38 @@
+package com.example.demo.pubsub;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.pubsub.v1.Publisher;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
+import com.google.pubsub.v1.TopicName;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class OrderPublisher {
+    public static void orderPublish(String message)
+            throws IOException, ExecutionException, InterruptedException {
+        String projectId = "arte-386819";
+        String topicId = "order-v1";
+        TopicName topicName = TopicName.of(projectId, topicId);
+
+        Publisher publisher = null;
+        try {
+            // Create a publisher instance with default settings bound to the topic
+            publisher = Publisher.newBuilder(topicName).build();
+
+            ByteString data = ByteString.copyFromUtf8(message);
+            PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
+
+            // Once published, returns a server-assigned message id (unique within the topic)
+            ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
+            String messageId = messageIdFuture.get();
+            System.out.println("Published message ID: " + messageId);
+        } finally {
+            if (publisher != null) {
+                // When finished with the publisher, shutdown to free up resources.
+                publisher.shutdown();
+                publisher.awaitTermination(1, TimeUnit.MINUTES);
+            }
+        }
+    }
+}
