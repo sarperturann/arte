2bb147d2a7c44850c126019609a8233ef885be8b
Mustafa Efe Tamyapar <efe.tamyapar@ug.bilkent.edu.tr>
2023-05-31 02:14:53 +0300

    Revert "email try cart"
    This reverts commit b47d202bd570e5e4037fc0e5c86a89b041badaeb.

--- server/src/main/java/com/example/demo/dao/ShoppingCart.java
+++ server/src/main/java/com/example/demo/dao/ShoppingCart.java
@@ -11,7 +11,7 @@ public class ShoppingCart {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CART_SEQUENCE")
     @Id
     private Long id;
-    private String userId;
+    private Long userId;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private long[] artworks;
