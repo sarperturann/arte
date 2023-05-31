bfd46b1c2efcc13dffe0a725f19c9121518460e3
Sarper Turan <sarper.turan01@gmail.com>
2023-05-31 16:27:53 +0300

    artwork add cart

--- client/src/components/Productpage/ProductpageDetail/ProductpageDetail.js
+++ client/src/components/Productpage/ProductpageDetail/ProductpageDetail.js
@@ -4,6 +4,9 @@ import { useSelector, useDispatch } from "react-redux/es/exports";
 import { cart } from "../../../actions/cartAction";
 import { toast } from "react-toastify";
 import Cookies from "universal-cookie";
+import axios from "axios";
+
+const gcloud = "https://arte-386819.uc.r.appspot.com";
 
 const ProductpageDetail = ({
   name,
@@ -18,34 +21,55 @@ const ProductpageDetail = ({
   id,
   primaryImg,
 }) => {
-  const dispatch = useDispatch();
-  const cookies = new Cookies();
-  let tkn = cookies.get("tkn");
-  const userState = useSelector((state) => state.userLogin);
-  const cartState = useSelector((state) => state.cart);
-  const cartedList = cartState.cart;
-  const [product, setProduct] = useState({
-    name: name,
-    id: id,
-    quanitity: 1,
-    primaryImg: primaryImg,
-    productPrice: price,
-  });
-  let qnt = product.quanitity;
-  const handleChange = (event) => {
-    setProduct({ ...product, quanitity: parseInt(event.target.value) });
-    qnt = event.target.value;
-  };
-
-  // on clicking add to cart
-  const cartHandler = () => {
-    // checking is user logged in or not
-    userState.authtokken === null && tkn === undefined
-      ? toast.warn("Please login to add Item !")
-      : cartedList.find((item) => item.id === product.id) === undefined
-      ? dispatch(cart("add", [...cartedList, product]))
-      : toast.info("Item already added");
-  };
+    const dispatch = useDispatch();
+    const cookies = new Cookies();
+    let tkn = cookies.get("tkn");
+    const userState = useSelector((state) => state.userLogin);
+    const cartState = useSelector((state) => state.cart);
+    const cartedList = cartState.cart;
+    const [product, setProduct] = useState({
+      name: name,
+      id: id,
+      quanitity: 1,
+      primaryImg: primaryImg,
+      productPrice: price,
+    });
+    let qnt = product.quanitity;
+    const handleChange = (event) => {
+      setProduct({ ...product, quanitity: parseInt(event.target.value) });
+      qnt = event.target.value;
+    };
+  
+    // on clicking add to cart
+    const cartHandler = async () => {
+      // checking if the user is logged in or not
+      if (userState.authtokken === null && tkn === undefined) {
+        toast.warn("Please login to add an item!");
+        return;
+      }
+  
+      // Check if the item is already in the cart
+      if (cartedList.find((item) => item.id === product.id) !== undefined) {
+        toast.info("Item already added");
+        return;
+      }
+  
+      try {
+        // Send the PUT request to add artwork to the cart
+        const response = await axios.put(gcloud + `/api/v1/carts/2/add_artwork?artworkId=${id}`);
+  
+        if (response.status === 200) {
+          // Dispatch the cart action to update the Redux store
+          dispatch(cart("add", [...cartedList, product]));
+          toast.success("Item added to cart!");
+        } else {
+          toast.error("Failed to add item to cart. Please try again later.");
+        }
+      } catch (error) {
+        console.error(error);
+        toast.error("An error occurred. Please try again later.");
+      }
+    };
   return (
     <div className="productPageDetail" style={{ lineHeight: "1.6", color: "#333" }}>
         <h1 style={{ marginBottom: "20px" }}>{name}</h1>
