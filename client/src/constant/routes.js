126565802e7a72f6b4878ffe3e87f4e059b66ba7
Mustafa Efe Tamyapar <efe.tamyapar@ug.bilkent.edu.tr>
2023-06-02 01:10:36 +0300

    changes2

--- client/src/constant/routes.js
+++ client/src/constant/routes.js
@@ -1,47 +1,47 @@
-const eway = "https://eway-server.vercel.app/api/";
+const temp = "https://eway-server.vercel.app/api/";
 const gcloud = "https://arte-386819.uc.r.appspot.com";
 
 // auth routes
-export const login = eway + 'users/login';
-export const signup = eway + 'users/signup';
-export const getuser = eway + 'users/getuser';
-export const updateuser = eway + 'users/updateuser/'; // user id required
+export const login = temp + 'users/login';
+export const signup = temp + 'users/signup';
+export const getuser = temp + 'users/getuser';
+export const updateuser = temp + 'users/updateuser/'; // user id required
 
 export const getMyUser = gcloud + 'user/get/'; // user id required
 export const createMyUserCart = gcloud % 'carts/create';
 export const getMyCart = gcloud % 'carts/'; //cart idididi
 // Search route
-export const searchKeyword = eway + 'search/byuser/'; // keyword in param required
+export const searchKeyword = temp + 'search/byuser/'; // keyword in param required
 
 // Contact us route
-export const contactUs = eway + 'contact/sendmessage';
+export const contactUs = temp + 'contact/sendmessage';
 
 // blog routes
-export const addblog = eway + 'blogs/addblog';
-export const getallblogs = eway + 'blogs/getallblogs';
-export const getblogbyid = eway + 'blogs/getblogbyid';
-export const updateblog = eway + 'blogs/updateblog/'; // blog id required
-export const deleteblog = eway + 'blogs/deleteblog/'; // blog id required
+export const addblog = temp + 'blogs/addblog';
+export const getallblogs = temp + 'blogs/getallblogs';
+export const getblogbyid = temp + 'blogs/getblogbyid';
+export const updateblog = temp + 'blogs/updateblog/'; // blog id required
+export const deleteblog = temp + 'blogs/deleteblog/'; // blog id required
 
 // product routes
-export const addproduct = eway + 'products/addblog';
-export const getallproducts = eway + 'products/getallproducts';
+export const addproduct = temp + 'products/addblog';
+export const getallproducts = temp + 'products/getallproducts';
 export const getallartworks = gcloud + '/api/v1/artwork/get_all';
 export const getAllArtworkImages = gcloud + "/api/v1/images/get_all";
 export const getCartbyIdBiz = gcloud + "/api/v1/carts/";
 export const getArtworkByIdBiz = gcloud + "/api/v1/artwork/get/"
-export const updateproduct = eway + 'products/updateproduct/'; // product id required
-export const deleteproduct = eway + 'products/deleteproduct/'; // product id required
+export const updateproduct = temp + 'products/updateproduct/'; // product id required
+export const deleteproduct = temp + 'products/deleteproduct/'; // product id required
 
 // cart routes
-export const addcart = eway + 'carts/createcartproduct';
-export const updatecart = eway + 'carts/updatecartproduct/'; // user id required
-export const getcartbyid = eway + 'carts/getcartproduct/'; // user id required
-export const removeProduct = eway + 'carts/deleteproduct/'; // user id and product id required :uid/:productId
+export const addcart = temp + 'carts/createcartproduct';
+export const updatecart = temp + 'carts/updatecartproduct/'; // user id required
+export const getcartbyid = temp + 'carts/getcartproduct/'; // user id required
+export const removeProduct = temp + 'carts/deleteproduct/'; // user id and product id required :uid/:productId
 
 // order routes
-export const placeorder = eway + 'userorder/placeorder'; // authentication required
-export const getorders = eway + 'userorder/getorders/'; // user id required in params + authentication required
-export const cancelorder = eway + 'userorder/cancelorder/'; // order id required in params + authentication required
+export const placeorder = temp + 'userorder/placeorder'; // authentication required
+export const getorders = temp + 'userorder/getorders/'; // user id required in params + authentication required
+export const cancelorder = temp + 'userorder/cancelorder/'; // order id required in params + authentication required
 
 export const getallordersBiz = gcloud + '/api/v1/order/get_all';
